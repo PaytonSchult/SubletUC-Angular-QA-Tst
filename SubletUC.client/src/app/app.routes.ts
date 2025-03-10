bf699f6f107dfe2f25830a7dcdad5b1b8dc794c4
Chris-Taylor7 <chtaylo270@gmail.com>
2025-03-10 18:33:22 -0400

    comment and warning cleanup

--- SubletUC.client/src/app/app.routes.ts
+++ SubletUC.client/src/app/app.routes.ts
@@ -1,48 +1,5 @@
 import { Routes } from '@angular/router';
 
-// export const routes: Routes = [
-//   {
-//     path: '',
-//     loadComponent: () => import('./modules/home/home/home.component').then(m => m.HomeComponent),
-//     title: 'home'
-//   },
-//   {
-//     path: 'home',
-//     loadComponent: () => import('./modules/home/home/home.component').then(m => m.HomeComponent),
-//     title: 'home'
-//   },
-//   {
-//     path: 'login',
-//     loadComponent: () => import('./modules/login/login/login.component').then(m => m.LoginComponent),
-//     title: 'login'
-//   },
-//   {
-//     path: 'create-listing',
-//     loadComponent: () => import('./modules/create-listing/create-listing/create-listing.component').then(m => m.CreateListingComponent),
-//     title: 'create-listing'
-//   },
-//   {
-//     path: 'profile',
-//     loadComponent: () => import('./modules/profile/profile/profile.component').then(m => m.ProfileComponent),
-//     title: 'profile'
-//   },
-//   {
-//     path: 'view-listings',
-//     loadComponent: () => import('./modules/view-listings/view-listings/view-listings.component').then(m => m.ViewListingsComponent),
-//     title: 'view-listings'
-//   },
-//   {
-//     path: 'messaging',
-//     loadComponent: () => import('./modules/messaging/messaging/messaging.component').then(m => m.MessagingComponent),
-//     title: 'messaging'
-//   },
-//   {
-//     path: 'bookmark',
-//     loadComponent: () => import('./modules/bookmarked/bookmarked/bookmarked.component').then(m => m.BookmarkedComponent),
-//     title: 'bookmark'
-//   }
-// ];
-
 export const routes: Routes = [
   {
     path: '',
@@ -54,6 +11,7 @@ export const routes: Routes = [
     loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
     title: 'home'
   },
+  //TODO: Implement Login functions and pages. Module is there, but no idea for a layout. Maybe use current azure auth?
   // {
   //   path: 'login',
   //   loadChildren: () => import('./modules/login/login/login.component').then(m => m.LoginComponent),
