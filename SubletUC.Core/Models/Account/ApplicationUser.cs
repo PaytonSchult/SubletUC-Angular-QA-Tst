3ba65bfb1b74ff26a0fb47c81827db134bdd2a6d
Chris_Taylor7 <chtaylor270@gmail.com>
2025-03-19 14:01:31 -0400

    fixing User Class

--- SubletUC.Core/Models/Account/ApplicationUser.cs
+++ SubletUC.Core/Models/Account/ApplicationUser.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using SubletUC.Server.Models;
 
 namespace SubletUC.Core.Models.Account
 {
-    public class ApplicationUser : IdentityUser, IAuditableEntity
+    public class ApplicationUser
     {
+        public int UserId { get; set; }
         public string? FullName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
         public bool IsActive { get; set; }
-        public bool IsLockedOut => LockoutEnabled && LockoutEnd >= DateTimeOffset.UtcNow;
+        //TODO: Implement with Login Service
+        // public bool IsLockedOut => LockoutEnabled && LockoutEnd >= DateTimeOffset.UtcNow;
         public string? CreatedBy { get; set; }
         public string? UpdatedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
         public ICollection<IdentityUserRole<string>> Roles { get; } = [];
+        public ICollection<Listing>? Listings { get; set; }
+        public ICollection<Listing>? Bookmarked { get; set; }
     }
 }
